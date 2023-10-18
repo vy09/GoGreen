16f6fb05e2a93f262904c9a94ba27835eb1e5cd3
VML8 <92667459+VML8@users.noreply.github.com>
2023-10-18 17:48:46 +0700

    Rework on some old code. Added button to use intent to go to the register view. Added register view.

---
+++ app/src/main/java/com/example/gogreen/ui/register/Register.kt
@@ -0,0 +1,12 @@
+package com.example.gogreen.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gogreen.R
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
